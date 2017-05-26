356084f71a593d6233a236b9bfe4fb5b31dfca8f
Peter Gribanov <wcode404@gmail.com>
2017-05-26 18:42:02 +0300

    test SpecificationQueryHandler

---
+++ tests/SpecificationQueryHandlerTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Query\Specification;
+
+use Doctrine\ORM\EntityManagerInterface;
+use GpsLab\Component\Query\Query;
+use Happyr\DoctrineSpecification\EntitySpecificationRepositoryInterface;
+use Happyr\DoctrineSpecification\Spec;
+
+class SpecificationQueryHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var SpecificationQueryHandler
+     */
+    private $handler;
+
+    protected function setUp()
+    {
+        $this->em = $this->getMock(EntityManagerInterface::class);
+        $this->handler = new SpecificationQueryHandler($this->em);
+    }
+
+    public function testHandleNotASpecificationQuery()
+    {
+        /* @var $query \PHPUnit_Framework_MockObject_MockObject|Query */
+        $query = $this->getMock(Query::class);
+
+        $this->em
+            ->expects($this->never())
+            ->method('getRepository')
+        ;
+
+        $this->assertNull($this->handler->handle($query));
+    }
+
+    public function testHandle()
+    {
+        $entity = 'foo';
+        $spec = Spec::andX();
+        $modifier = Spec::cache(3600);
+        $result = 'bar';
+
+        /* @var $query \PHPUnit_Framework_MockObject_MockObject|Query */
+        $query = $this->getMock(SpecificationQuery::class);
+        $query
+            ->expects($this->once())
+            ->method('entity')
+            ->will($this->returnValue($entity))
+        ;
+        $query
+            ->expects($this->once())
+            ->method('spec')
+            ->will($this->returnValue($spec))
+        ;
+        $query
+            ->expects($this->once())
+            ->method('modifier')
+            ->will($this->returnValue($modifier))
+        ;
+
+        /* @var $rep \PHPUnit_Framework_MockObject_MockObject|EntitySpecificationRepositoryInterface */
+        $rep = $this->getMock(EntitySpecificationRepositoryInterface::class);
+        $rep
+            ->expects($this->once())
+            ->method('match')
+            ->with($spec, $modifier)
+            ->will($this->returnValue($result))
+        ;
+
+        $this->em
+            ->expects($this->once())
+            ->method('getRepository')
+            ->with($entity)
+            ->will($this->returnValue($rep))
+        ;
+
+        $this->assertEquals($result, $this->handler->handle($query));
+    }
+}
